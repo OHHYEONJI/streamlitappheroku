654f3670035a44a928e3435f2263c8644c7843ca
OHHYEONJI <78291206+OHHYEONJI@users.noreply.github.com>
2022-12-18 17:56:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-from apps import home, individualprediction, contact_us, how_to_use
+
 
 # loading navigation bar stylesheet
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
@@ -23,7 +23,3 @@ st.markdown("""
 </nav>
 """, unsafe_allow_html=True)
 
-PAGES = {"Home":home, "How to use":how_to_use, "Individual prediction":individualprediction, "Contact us":contact_us}
-selection = st.radio('Go to', list(PAGES.keys()))
-page = PAGES[selection]
-page.app()
